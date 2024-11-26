8ddfcbf6f715e9769cc7467526375a8e96636b49
liuziyuan1109 <928701846@qq.com>
2024-11-26 18:49:26 +0800

    设置了前端环境变量

--- seckill-frontend-main/src/src/api/user.js
+++ seckill-frontend-main/src/src/api/user.js
@@ -1,7 +1,7 @@
 // src/api/user.js
 import axios from "axios";
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:28080"; // 后端服务地址
+const API_BASE_URL = process.env.VUE_APP_API_BASE_URL; // 后端服务地址
 
 
 export async function register(username, password) {
--- seckill-frontend-main/src/src/views/ProductDetail.vue
+++ seckill-frontend-main/src/src/views/ProductDetail.vue
@@ -59,7 +59,7 @@
 </template>
 
 <script>
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:28080"
+const API_BASE_URL = process.env.VUE_APP_API_BASE_URL
 export default {
     name: 'ProductDetail',
     data() {
